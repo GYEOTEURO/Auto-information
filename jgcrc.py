88c341cd79470ab8893ebd3962f17f1e1aa6f7e2
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-02 17:39:35 +0900

    Feat: [BY-172] Period check

--- jgcrc.py
+++ jgcrc.py
@@ -11,7 +11,8 @@ before_one_month = now_date + relativedelta(months=-1)
 
 print(str(before_one_month))
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+s = Service('./chromedriver')
+driver = webdriver.Chrome(service=s)
 
 url = "https://www.jgcrc.or.kr/notice"
 driver.get(url)
@@ -24,9 +25,23 @@ current_date = datetime.date.today()
 # Collect the post links
 post_links = []
 for row in rows:
-    link_element = row.find_element(By.TAG_NAME, 'a')
-    link = link_element.get_attribute('href')
-    post_links.append(link)
+    try:
+        # 포스트의 날짜 정보 추출
+        date_string = row.find_element(By.CLASS_NAME, 'date').text
+        post_date = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()
+
+        # 날짜 차이 계산 (오늘과 포스트 작성일과의 차이)
+        date_diff = (current_date - post_date).days
+
+        # 포스트가 30일 이내에 작성된 것인지 확인
+        if date_diff <= 30:
+            # 30일 이내에 작성된 포스트의 경우 링크를 수집
+            link_element = row.find_element(By.TAG_NAME, 'a')
+            link = link_element.get_attribute('href')
+            post_links.append(link)
+
+    except Exception as e:
+        print(f"날짜 처리 중 오류 발생: {e}")
 
 # Iterate over the post links and extract the data for the posts within the last month
 for link in post_links:
@@ -34,44 +49,35 @@ for link in post_links:
         # Navigate to the link
         driver.get(link)
 
-        # Extract the post date from the internal page
-        date_string = driver.find_element(By.CLASS_NAME, 'view_info').text
-        post_date = datetime.datetime.strptime(date_string.split('작성일')[1].strip(), '%Y.%m.%d').date()
+        # Extract the content and other optional elements from the internal page
+        title = driver.find_element(By.CLASS_NAME, 'title_view').text
+        author_date = driver.find_element(By.CLASS_NAME, 'view_info').text
+        content = driver.find_element(By.CLASS_NAME, 'view_detail').text
 
-        # Calculate the date difference in days
-        date_diff = (current_date - post_date).days
+        # Extract the image URL if it exists
+        try:
+            image_element = driver.find_element(By.CLASS_NAME, 'view_image')
+            image_url = image_element.get_attribute('href')
+        except :
+            image_url = None
 
-        # Check if the post is within the last month
-        if date_diff <= 30:
-            # Extract the content and other optional elements from the internal page
-            title = driver.find_element(By.CLASS_NAME, 'title_view').text
-            author_date = driver.find_element(By.CLASS_NAME, 'view_info').text
-            content = driver.find_element(By.CLASS_NAME, 'view_detail').text
+        # Extract other optional elements if they exist
+        try:
+            attachment_element = driver.find_element(By.CSS_SELECTOR, 'ul.ul_view_01 li em')
+            attachment_name = attachment_element.text
+            attachment_link = attachment_element.find_element(By.XPATH, './following-sibling::span/a').get_attribute('href')
+        except :
+            attachment_name = None
+            attachment_link = None
 
-            # Extract the image URL if it exists
-            try:
-                image_element = driver.find_element(By.CLASS_NAME, 'view_image')
-                image_url = image_element.get_attribute('href')
-            except :
-                image_url = None
-
-            # Extract other optional elements if they exist
-            try:
-                attachment_element = driver.find_element(By.CSS_SELECTOR, 'ul.ul_view_01 li em')
-                attachment_name = attachment_element.text
-                attachment_link = attachment_element.find_element(By.XPATH, './following-sibling::span/a').get_attribute('href')
-            except :
-                attachment_name = None
-                attachment_link = None
-
-            # Print the extracted data
-            print(f"제목: {title}")
-            print(f"작성자 및 날짜: {author_date}")
-            print(f"내용: {content}")
-            print(f"이미지 URL: {image_url}")
-            print(f"첨부 파일: {attachment_name}")
-            print(f"첨부 파일 링크: {attachment_link}")
-            print("--------------------")
+        # Print the extracted data
+        print(f"제목: {title}")
+        print(f"작성자 및 날짜: {author_date}")
+        print(f"내용: {content}")
+        print(f"이미지 URL: {image_url}")
+        print(f"첨부 파일: {attachment_name}")
+        print(f"첨부 파일 링크: {attachment_link}")
+        print("--------------------")
 
     except Exception as e:
         print(f"링크 처리 중 오류 발생: {e}")
