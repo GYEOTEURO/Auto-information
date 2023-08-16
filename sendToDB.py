a79870118c8124d294c56f6b12e31404d94612a0
anjiwon319 <anjiwon319@kookmin.ac.kr>
2023-08-16 12:38:54 +0900

    [BY-196] Feat: 요약문 클래스 인스턴스 생성 후 DB 전송 과정 코드화 완료

--- sendToDB.py
+++ sendToDB.py
@@ -2,28 +2,44 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import pandas as pd
-
+from summarizer import Summarizer
+import os
 
 
 # Use the application default credentials
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
-# autoInformation(collection) - 카테고리명(doc) - posts(collection) - 개별 post(doc)
+# 요약문 df 를 가지고 firestore DB로 보내는 함수
+def sendToDB(df):
+    pass
 
-doc_ref = db.collection(u'autoInformation').document(u'{csv 파일에서 가져온 카테고리명}')
+# 파일들 이름 가져오기
+def getSummarizedFileNames():
+    return os.listdir("result/summary")
 
-# 유의미한 ID를 두지 않고 Cloud Firestore에서 자동으로 ID를 생성 : add()
-doc_ref.collection("posts").add({
-    u'title': None,
-    u'content': None,
-    u'original_link': None,
-    u'content_link': None,
-    u'image': None,
-    u'region': None,
-    u'date': None,
-    u'site' : None,
-    u'disabillity_type': None,
-})
+
+# Summarize 인스턴스 생성
+bardSummarizer = Summarizer("Bard")
+
+# 반복문으로 result/summary에 있는 csv 파일 하나씩 가져오면서 summarize 진행
+    # summarize 진행 후 df 가져와서 DB로 보내기
+files = getSummarizedFileNames()
+for file in files:
+    fileName = file[:-4]
+    bardSummarizer.setFileName(fileName)
+    bardSummarizer.summarizeContents()
+    
+    bardSummarizer.df.drop(['Unnamed: 0'], axis = 1, inplace = True)
+    print(bardSummarizer.df)
+
+    for i in bardSummarizer.df.index:
+        data = bardSummarizer.df.loc[i].to_dict()
+        print(data)
+
+        # autoInformation(collection) - 카테고리명(doc) - posts(collection) - 개별 post(doc)
+        doc_ref = db.collection('autoInformation').document(data['category']).collection('posts').document()
+
+        # 유의미한 ID를 두지 않고 Cloud Firestore에서 자동으로 ID를 생성 : add() , document().set()
+        doc_ref.set(data)
