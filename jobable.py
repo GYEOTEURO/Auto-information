fda5251987836d86b1b06bff8542dc90b3953373
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-21 18:56:07 +0900

    [BY-206] Feat: complete except image

--- jobable.py
+++ jobable.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import datetime
 from dateutil.relativedelta import relativedelta
@@ -40,7 +41,6 @@ except Exception as e:
 now_date = datetime.datetime.now()
 before_one_month = now_date + relativedelta(months=-1)
 
-print(str(before_one_month))
 
 s = Service('./chromedriver')
 driver = webdriver.Chrome(service=s)
@@ -57,73 +57,64 @@ current_date = datetime.date.today()
 post_links = []
 for row in rows:
     try:
-        columns = row.find_elements(By.TAG_NAME, 'td')
-
-        if len(columns) >= 5:
-            date_string = columns[3].text.strip()  # Remove whitespace
-            post_date = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()  # Fixed date format
-
-            # Calculate date difference
-            date_diff = (current_date - post_date).days
-
-            if date_diff <= 30:
-                link_element = columns[1].find_element(By.TAG_NAME, 'a')
-                link = link_element.get_attribute('href')
-                post_links.append(link)
-
+        # JavaScript 함수 호출하여 링크 얻기
+        link = driver.execute_script("return arguments[0].querySelector('a')?.getAttribute('href');", row)
+        
+        # 링크가 있는 경우에만 처리
+        if link:
+            # 숫자 추출
+            numbers = re.findall(r'\d+', link)
+            if numbers:
+                bbscttSn = numbers[0]
+                # 링크 생성
+                post_link = f"https://jobable.seoul.go.kr/jobable/custmr_cntr/ntce/WwwNotice.do?method=getWwwNotice&chUseZe=D&noticeCmmnSeNo=1&bbscttSn={bbscttSn}&fileyn=N"
+                post_links.append(post_link)
+        
     except Exception as e:
-        print(f"Error processing date: {e}")
+        print(f"Error processing row: {e}")
+
 
 # Iterate over the post links and extract the data for the posts within the last month
 for link in post_links:
+    image_urls = []
+
     try:
-        # Navigate to the link
         driver.get(link)
 
-        # Extract the title, author, date, and content from the internal page
+        # 제목 추출
         try:
-            title_element = driver.find_element(By.XPATH, "//h2[@id='bo_v_title']")
+            title_element = driver.find_element(By.XPATH, "//th[@class='title']")
             title = title_element.text
         except Exception as e:
             print(e, ": title crawling failed")
             title = ""
 
+        # 날짜 추출
         try:
-            date_element = driver.find_element(By.XPATH, "//section[@id='bo_v_info']//span[contains(@class, 'sound_only') and contains(text(), '작성일')]")
-            date_string = date_element.find_element(By.XPATH, "./following-sibling::strong").text.split()[0]
+            date_elements = driver.find_elements(By.XPATH, "//tr/th[text()='등록일']/following-sibling::td")
+            date_str = date_elements[0].text
+            date = datetime.datetime.strptime(date_str, '%d.%m.%y').strftime('%d-%m-%y')
         except Exception as e:
-            print(e, "date crawling failed")
-            date_string = ""
+            print(e, ": date crawling failed")
+            date = ""
 
+
+        # 이미지 URL 추출
         try:
-            content_element = driver.find_element(By.XPATH, "//div[@id='bo_v_con']")
+            image_elements = driver.find_elements(By.XPATH, "//div[@class='view']/img")
+            image_urls = [element.get_attribute("src") for element in image_elements]
+        except Exception as e:
+            print(e, ": image URL crawling failed")
+            image_urls = []
+
+
+        # 글 내용 추출
+        try:
+            content_element = driver.find_element(By.XPATH, "//td[@class='view']")
             content = content_element.text
         except Exception as e:
-            print(e, "content crawling failed")
+            print(e, ": content crawling failed")
             content = ""
-            
-        try:
-            image_urls = []
-            
-            # If multiple images are inside <div id="bo_v_con">
-            img_elements = driver.find_elements(By.CSS_SELECTOR, "#bo_v_con img")
-            image_urls.extend([img.get_attribute("src") for img in img_elements if "newsletter" in img.get_attribute("src")])
-
-            # Try to find images inside <div id="bo_v_con">
-            img_elements = driver.find_elements(By.CSS_SELECTOR, "#bo_v_con img")
-            image_urls.extend([img.get_attribute("src") for img in img_elements if "data/editor" in img.get_attribute("src")])
-
-            # Try to find an image inside <div id="bo_v_img">
-            try:
-                image_element = driver.find_element(By.XPATH, "//div[@id='bo_v_img']//a[@class='view_image']")
-                image_url = image_element.find_element(By.TAG_NAME, "img").get_attribute('src')
-                image_urls.append(image_url)
-            except:
-                pass
-
-        except Exception as e:
-            print(e, "Image crawling failed")
-        
 
         # Append the extracted data to lists
         sites.append(siteName)
@@ -131,9 +122,9 @@ for link in post_links:
         categories.append(category)
         disabilityTypes.append(disabilityType)
         titles.append(title)
-        dates.append(date_string)
+        dates.append(date)
         contents.append(content)
-        images.append(image_urls)
+        images.append(image_urls) 
         originalLinks.append(link)
 
     except Exception as e:
