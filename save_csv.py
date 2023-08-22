bfa5ca62c96fc6378a06124e49265095f4e17e98
anjiwon319 <anjiwon319@kookmin.ac.kr>
2023-08-22 18:56:52 +0900

    Chore: 절대 경로를 상대경로로 변경

--- save_csv.py
+++ save_csv.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-path_folder = '/home/shinmg/2022barrier-free/autoInformation/Auto-information/result/'
-
 def saveCsv(file_name, sites, regions, categories, disabilityTypes, titles, dates, contents, contentLinks, images):
-    df = pd.read_csv(f'{path_folder}format.csv', encoding='utf-8')
-    df.to_csv(f'{path_folder}crawl/{file_name}.csv', mode='w', encoding='utf-8')
+    df = pd.read_csv(f'result/format.csv', encoding='utf-8')
+    df.to_csv(f'result/crawl/{file_name}.csv', mode='w', encoding='utf-8')
 
     df['site'] = sites
     df['region'] = regions
@@ -18,4 +16,4 @@ def saveCsv(file_name, sites, regions, categories, disabilityTypes, titles, date
     df['image'] = images
         
 
-    df.to_csv(f'{path_folder}crawl/{file_name}.csv', mode='a', header=False, encoding='utf-8')
+    df.to_csv(f'result/crawl/{file_name}.csv', mode='a', header=False, encoding='utf-8')
