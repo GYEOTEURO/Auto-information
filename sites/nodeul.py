d9a7a09f1f449ecaf799b1e356d79884047c9791
anjiwon319 <anjiwon319@kookmin.ac.kr>
2023-08-26 01:04:18 +0900

    [BY-192] Feat: content, image, date 제외하고 크롤링 완료

--- sites/nodeul.py
+++ sites/nodeul.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class Nodeul(Crawl):
 
 
-    def __init__(self, url, latestCrawlDate='2023-06-01') -> None:
+    def __init__(self, url, latestCrawlDate='2023-01-01') -> None:
         super().__init__("nodeul", url, latestCrawlDate)
 
     def movetoNextPage(self) -> None:
@@ -16,25 +16,25 @@ class Nodeul(Crawl):
         self.driver.get(nextPageUrl)
 
     def getOutsideDate(self, thread) -> datetime:
-        date = thread.find_element(By.CLASS_NAME, "div > div.info > span:nth-child(1) > b").text
-        date = datetime.strptime(date, '%Y.%m.%d')
+        try:
+            date = thread.find_element(By.CSS_SELECTOR, "div > div.info > span:nth-child(1) > b").text
+            date = datetime.strptime(date, '%Y.%m.%d')
+        except Exception as e:
+            print(e, ": 리스트뷰에서 게시글 바깥의 날짜 가져오기 실패")
+            date = datetime.now()
         return date
-    
 
     def isRemaiedPaegs(self) -> bool:
         try:
             firstThreadOfPage = self.driver.find_elements(By.CSS_SELECTOR, "#bd_224_0 > div.bd_lst_wrp > ol > li")[0]
             dateOfFirstContent = self.getOutsideDate(firstThreadOfPage)
-            if dateOfFirstContent <= self.latestCrawlDate:
+            if dateOfFirstContent >= self.latestCrawlDate:
                 return True
-            else:
-                return False
             
         except Exception as e:
             print(e, ": 크롤링할 페이지가 남았는 지 확인하는 코드 실행 실패")
         
         return False
-
     
     def makeContentLinkGroup(self) -> None:
         self.contentLinks = []
@@ -53,8 +53,90 @@ class Nodeul(Crawl):
                 break
 
         return None
+    
+    def isSecretContent(self, thread):
+        try:
+            content = thread.find_element(By.CLASS_NAME, "cnt").text
+            if content == "비밀글입니다.":
+                return True
+        
+        except Exception as e:
+            print(e, ": 해당 게시글이 비밀 글인지에 대한 판단 실패")
+        
+        return False
+    
+    def appendContentLinkOf(self, threads):
+        for thread in threads:
+            date = self.getOutsideDate(thread)
+            if date <= self.latestCrawlDate:
+                return False
+            else:
+                contentLink = self.getContentLink(thread)
+                print(f"{date} : {contentLink}")
 
+                if not self.isSecretContent(thread):
+                    self.contentLinks.append(contentLink)
+        return True
+
+    def getSite(self):
+        return "노들장애인자립센터"
+        
+    def getTitle(self):
+        try:
+            title = self.driver.find_element(By.CLASS_NAME, "np_18px").find_element(By.TAG_NAME, 'a').text
+        except Exception as e:
+            print(e, ": title 가져오기 실패")
+            title = ''
+
+        return title
+    
+    def getContent(self):
+        content = ''
+        try:
+            threadsOfContent = self.driver.find_element(By.CLASS_NAME, "rd_body clear")[0].find_elements(By.TAG_NAME, 'p')
+            for thread in threadsOfContent:
+                content += thread.text + '\n'
+
+        except Exception as e:
+            print(e, ": content 가져오기 실패")
+            print('content:', content)
+            content = ''
+
+        content = self.removeEmailAddress(content)
+        return content
+    
+    def removeEmailAddress(self, content):
+        content = content.replace('nodl@hanmail.net', '노들장애인자립생활센터 이메일 주소')
+        return content
+        
+    def getImage(self):
+        images = []
+        try:
+            threadsOfImage = self.driver.find_element(By.CLASS_NAME,"rd_body clear")[0].find_elements(By.TAG_NAME, 'img')
+            for thread in threadsOfImage:
+                imageLink = thread.getAttridute('src')
+                images.append(imageLink)
+        except Exception as e:
+            print(e, ": image 가져오기 실패")
+        
+        return images
+    
+    def getInsideDate(self):
+        try:
+            date = self.driver.find_element(By.CLASS_NAME, "date m_no").text
+            date = datetime.strptime(date, '%Y.%m.%d %H:%M')
+        except Exception as e:
+            print(e, ": 게시글 안의 날짜 가져오기 실패")
+            date = datetime.now()
+        return date
+    
+    def getRegion(self):
+        return super().getRegion()
+    
+    def getDisabilityType(self):
+        return "전체"
 
 
 if __name__ == "__main__":
     nodeul = Nodeul("http://ncil.or.kr/events_news")
+    nodeul.startCrawl()
