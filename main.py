6cb25b6e532bd789ac3e6ffb646b303951b0e8d0
anjiwon319 <anjiwon319@kookmin.ac.kr>
2023-08-29 18:53:43 +0900

    [BY-259] Fix: 한글 적용되게끔 인코딩 조건 추가해서 코드 재작성

--- main.py
+++ main.py
@@ -10,25 +10,26 @@ with open('constants.yaml', encoding='UTF-8') as f:
 
 def updateLatestDate():
   constants['crawl']['latest_date'] = datetime.now().strftime("%Y-%m-%d")
-  with open('constants.yaml', 'w') as f:
-    yaml.dump(constants, f)
+  with open('constants.yaml', 'w', encoding='utf-8') as f:
+    yaml.dump(constants, f, allow_unicode=True)
 
 def runCrawling():
-  crawlFiles = os.listdir("sites").remove('crawl.py')
+  crawlFiles = os.listdir("sites")
+  crawlFiles.remove('crawl.py')
   crawlFiles.remove('save_csv.py')
   print(crawlFiles)
 
-  for crawlFile in crawlFiles:
-    print("-------------------")
-    print(crawlFile, "실행 시작")
-    try:
-      subprocess.run(["python", crawlFile])
-      print(crawlFile, "실행 완료")
+  # for crawlFile in crawlFiles:
+  #   print("-------------------")
+  #   print(crawlFile, "실행 시작")
+  #   try:
+  #     subprocess.run(["python", crawlFile])
+  #     print(crawlFile, "실행 완료")
 
-    except Exception as e:
-      print(e, crawlFile, "실행 실패")
+  #   except Exception as e:
+  #     print(e, crawlFile, "실행 실패")
     
-  updateLatestDate()
+  # updateLatestDate()
 
 if __name__ == "__main__":
     runCrawling()
